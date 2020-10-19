49c165217ea80c1caa5ad6f59b1bbff080c2fc40
yuyakodan <yuya.kodan@gmail.com>
2020-10-19 20:45:07 +0900

    First pages commit

--- sw.js
+++ sw.js
@@ -1,37 +1,59 @@
-var urlsToCache = [
-    "index.html",
-];
 
-var CACHE_NAME = 'cache-v1';
-
-const CACHE_KEYS = [
-    CACHE_NAME
-];
-
+var CACHE_NAME = 'sample-v7';
+var urlsToCache = ['/index.html'];
 
 self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open(CACHE_NAME) // 上記で指定しているキャッシュ名
-            .then(
-                function(cache){
-                    return cache.addAll(urlsToCache); // 指定したリソースをキャッシュへ追加
-                    // 1つでも失敗したらService Workerのインストールはスキップされる
-                })
+        caches.open(CACHE_NAME).then(async function(cache) {
+            skipWaiting();
+            console.log(urlsToCache);
+            console.log('をキャッシュします');
+            cache.addAll(urlsToCache);
+        })
     );
 });
 
-
-self.addEventListener('activate', event => {
+self.addEventListener('activate', function(event) {
     event.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(
-                keys.filter(key => {
-                    return !CACHE_KEYS.includes(key);
-                }).map(key => {
-                    // 不要なキャッシュを削除
-                    return caches.delete(key);
-                })
-            );
+        (function() {
+            caches.keys().then(function(oldCacheKeys) {
+                console.log(oldCacheKeys);
+                oldCacheKeys
+                    .filter(function(key) {
+                        return key !== CACHE_NAME;
+                    })
+                    .map(function(key) {
+                        console.log(key + 'を削除しました');
+                        return caches.delete(key);
+                    });
+            });
+            clients.claim();
+        })()
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) {
+                console.log(event.request.url + 'はキャッシュがあるので使う');
+                return response;
+            }
+            console.log(event.request.url + 'はキャッシュが無いのでリクエストする');
+
+            var fetchRequest = event.request.clone();
+            return fetch(fetchRequest).then(function(response) {
+                if (!response || response.status !== 200 || response.type !== 'basic') {
+                    return response;
+                }
+
+                var responseToCache = response.clone();
+                console.log(event.request.url + 'は新しくキャッシュに保管する');
+                caches.open(CACHE_NAME).then(function(cache) {
+                    cache.put(event.request, responseToCache);
+                });
+                return response;
+            });
         })
     );
 });
